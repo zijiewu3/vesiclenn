cda62e0337e9bf8eead86501542a46dc69e50954
zijiewu3 <zijiewu@udel.edu>
2022-02-13 00:56:43 -0500

    pruned extra files

--- cga_vesiclenn/cga_vesicle2nn/__init__.py
+++
@@ -1,4 +0,0 @@
-#from cga-bbvesicles.scatterer_generator import scatterer_generator 
-from pathlib import Path
-PACKAGEDIR = Path(__file__).parent.absolute()
-NNMODEL_DIR_PATH = str(PACKAGEDIR / 'nn_models/')
