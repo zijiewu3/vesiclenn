a2b7636c6a5080160f0fa5445f214b2791a05657
zijiewu3 <zijiewu@udel.edu>
2022-02-12 23:41:41 -0500

    add vesiclenn_2parts

--- cga_vesiclenn/cga_vesicle2nn/scatterer_generator.py
+++ cga_vesiclenn/cga_vesicle2nn/scatterer_generator.py
@@ -14,7 +14,8 @@ class scatterer_generator:
     ------------------------------------------
     *TODO*
     '''
-    def __init__(self,shape_params = [NNMODEL_DIR_PATH+'/1.h5'],
+    def __init__(self,
+                 shape_params = [NNMODEL_DIR_PATH+'/1.h5'],
                  minvalu = (50,0,0,0,0,0,0,2),
                  maxvalu = (2000,1,1,1,1,1,1,7)):
         self.minvalu = minvalu
@@ -48,21 +49,22 @@ class scatterer_generator_2parts:
     ------------------------------------------
     *TODO*
     '''
-    def __init__(self,shape_params = [NNMODEL_DIR_PATH+'/1.h5',NNMODEL_DIR_PATH+'/2.h5',qRbreak],
+    def __init__(self,
+                 shape_params = [NNMODEL_DIR_PATH+'/1.h5',NNMODEL_DIR_PATH+'/2.h5',qRbreak],
                  minvalu = (50,0,0,0,0,0,0,2),
                  maxvalu = (2000,1,1,1,1,1,1,7)):
         self.minvalu = minvalu
         self.maxvalu = maxvalu
         self.numvars = 8
-        self.model_path_left = shape_params[0]
-        self.model_path_right = shape_params[1]
+        self.left_model_path = shape_params[0]
+        self.right_model_path = shape_params[1]
         self.qRbreak = qRbreak
 
     def load_model(self):
-        self.model_left = tf.keras.models.load_model(self.model_path_left)
+        self.left_model = tf.keras.models.load_model(self.left_model_path)
+        self.right_model = tf.keras.models.load_model(self.right_model_path)
         self.nn_minvalu_left = np.array([0,0,0,0,0,0,-1])
         self.nn_maxvalu_left = np.array([1,1,1,1,1,1,2])
-        self.model_right = tf.keras.models.load_model(self.model_path_right)
         self.nn_minvalu_right = np.array([0,0,0,0,0,0,5])
         self.nn_maxvalu_right = np.array([1,1,1,1,1,1,10])
 
@@ -70,18 +72,22 @@ class scatterer_generator_2parts:
         self.load_model()
         param = np.array(param)
         R = param[0]
-        qRrange = np.array(qrange)*R
+        qrange = np.array(qrange)
+        qRrange = qrange*R
         qRrange_left = qRrange[np.where(qRrange < self.qRbreak)]
         qRrange_right = qRrange[np.where(qRrange >= self.qRbreak)]
-        nn_input_left = np.zeros((len(qrange_left),7))
-        nn_input_right = np.zeros((len(qrange_right),7))
-        nn_input_left[:,6] = (np.log10(qRrange_left)-self.nn_minvalu_left[-1])/(self.nn_maxvalu_left[-1]-self.nn_minvalu_left[-1])
-        nn_input_right[:,6] = (qRrange_right-self.nn_minvalu_right[-1])/(self.nn_maxvalu_right[-1]-self.nn_minvalu_right[-1])
+        nn_input_left = np.zeros((len(qRrange_left),7))
+        nn_input_right = np.zeros((len(qRrange_right),7))
         nn_input_left[:,:6] = (param[1:7]-self.nn_minvalu_left[:6])/(self.nn_maxvalu_left[:6]-self.nn_minvalu_left[:6])
         nn_input_right[:,:6] = (param[1:7]-self.nn_minvalu_right[:6])/(self.nn_maxvalu_right[:6]-self.nn_minvalu_right[:6])
-        nn_output_left = np.array([10**i for i in self.model(nn_input_left).numpy()]).flatten()
-        nn_output_right = np.array([10**i for i in self.model(nn_input_right).numpy()]).flatten()
-        nn_ouput = np.hstack((nn_output_left,nn_output_right))
+
+        nn_input_left[:,6] = (np.log10(qRrange_left)-self.nn_minvalu_left[-1])/(self.nn_maxvalu_left[-1]-self.nn_minvalu_left[-1])
+        nn_input_right[:,6] = (qRrange_right-self.nn_minvalu_right[-1])/(self.nn_maxvalu_right[-1]-self.nn_minvalu_right[-1])
+        
+        nn_output_left = np.array([10**i for i in self.left_model(nn_input_left).numpy()]).flatten()
+        nn_output_right = np.array([10**i for i in self.right_model(nn_input_right).numpy()]).flatten()
+        nn_output = np.hstack((nn_output_left,nn_output_right))
         nn_output = nn_output + 10**(-param[-1])
         return nn_output/nn_output[0]
- 
+            
+
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
         name="cga_vesiclenn",
         install_requires="crease_ga",
-        entry_points={"crease_ga.plugins":["vesiclenn=cga_vesiclenn.scatterer_generator:scatterer_generator"]},
+        entry_points={"crease_ga.plugins":["vesiclenn=cga_vesiclenn.scatterer_generator:scatterer_generator",
+                                           "vesiclenn_2parts = cga_vesiclenn.scatter_generator:scatterer_generator_2parts"]},
         py_modules=["cga_vesiclenn"],
                         )
