dc982b75966335393371fefbda8bfc5540495139
Zijie Wu <zijiewu@r0login1.localdomain.hpc.udel.edu>
2022-06-08 17:03:34 -0400

    include disp_limitta

---
+++ cga_vesiclenn_disp_limitta/scatterer_generator.py
@@ -0,0 +1,159 @@
+import crease_ga as cga
+import numpy as np
+import sys
+from tensorflow import keras
+import tensorflow as tf
+from cga_vesiclenn import NNMODEL_DIR_PATH
+def gaussian(x,mu,sigma):
+    return 1/np.sqrt(2*np.pi*sigma**2)*np.exp(-(x-mu)**2/(2*sigma**2))
+class scatterer_generator:
+    '''
+    shape specific descriptors (shape_params):
+    ------------------------------------------
+    *None.*
+
+    Input parameters to be predicted:
+    ------------------------------------------
+    *TODO*
+    '''
+    def __init__(self,shape_params = [NNMODEL_DIR_PATH+'/1.h5',0.6],
+                 minvalu = (150, 65,  0.2,1.0, 0.1, 1),#Rtotal tb/ta rhoAin/rhoAout pd bg
+                 maxvalu = (3000,85, 3.0,2.0, 1.0, 5)):
+        self.minvalu = minvalu
+        self.maxvalu = maxvalu
+        self.numvars = 6
+        self.model_path = shape_params[0]
+        self.sB = shape_params[1]
+
+    def load_model(self):
+        self.model = tf.keras.models.load_model(self.model_path)
+        self.nn_minvalu = np.array([0,0,0,0.5,0.5,np.log(0.5)])
+        self.nn_maxvalu = np.array([1,1,1,1,0.75,np.log(10)])
+
+    def converttoIQ(self,qrange,param):
+        self.load_model()
+        param = np.array(param)
+        R_total_mu = param[0]
+        tAin = param[1]
+        tAout = tAin
+        tB = tAin*param[2]
+         
+        R_core_mu = R_total_mu-tAin-tAout-tB
+        
+        R_core_sd = R_core_mu*param[-2]
+        nn_output_sum = np.zeros(len(qrange))
+        if R_core_mu < 0:
+            return nn_output_sum + 1
+        for Rcore in np.linspace(R_core_mu-1.5*R_core_sd, R_core_mu+1.5*R_core_sd,num = 20):
+            if Rcore < 0:
+                continue
+            Rtotal = Rcore+tAin+tAout+tB
+            qRrange = qrange*Rtotal
+            fcore = Rcore/Rtotal
+            fAin = tAin/(Rtotal-Rcore)
+            fAout = tAout/(tAout+tB)
+            sAin = param[3]
+
+            nn_input = np.zeros((len(qrange),6))
+            input_nn = [fcore,fAin,fAout,sAin,self.sB]
+            nn_input[:,5] = (np.log10(qRrange)-self.nn_minvalu[-1])/(self.nn_maxvalu[-1]-self.nn_minvalu[-1])
+            nn_input[:,:5] = (input_nn-self.nn_minvalu[:5])/(self.nn_maxvalu[:5]-self.nn_minvalu[:5])
+            nn_output_sum += np.array([10**i for i in self.model(nn_input).numpy()]).flatten()*gaussian(Rcore,R_core_mu,R_core_sd)
+        nn_output_sum /= nn_output_sum[0]
+        nn_output_sum += 10**(-param[-1])
+        return nn_output_sum
+            
+class scatterer_generator_log_normal:
+    '''
+    shape specific descriptors (shape_params):
+    ------------------------------------------
+    *None.*
+
+    Input parameters to be predicted:
+    ------------------------------------------
+    *TODO*
+    '''
+    def __init__(self,shape_params = [NNMODEL_DIR_PATH+'/1.h5',NNMODEL_DIR_PATH+'/2.h5',5],
+                 minvalu = (50,0,0,0,0,0,0,6),
+                 maxvalu = (2000,1,1,1,1,1,1,7)):
+        self.minvalu = minvalu
+        self.maxvalu = maxvalu
+        self.numvars = 8
+        self.model_path_left = shape_params[0]
+        self.model_path_right = shape_params[1]
+        self.qRbreak = shape_params[2]
+
+    def load_model(self):
+        self.model_left = tf.keras.models.load_model(self.model_path_left)
+        self.nn_minvalu_left = np.array([0,0,0,0,0,0,-1])
+        self.nn_maxvalu_left = np.array([1,1,1,1,1,1,2])
+        self.model_right = tf.keras.models.load_model(self.model_path_right)
+        self.nn_minvalu_right = np.array([0,0,0,0,0,0,5])
+        self.nn_maxvalu_right = np.array([1,1,1,1,1,1,10])
+
+    def converttoIQ(self,qrange,param):
+        self.load_model()
+        param = np.array(param)
+        R = param[0]
+        qRrange = np.array(qrange)*R
+        qRrange_left = qRrange[np.where(qRrange < self.qRbreak)]
+        qRrange_right = qRrange[np.where(qRrange >= self.qRbreak)]
+        nn_input_left = np.zeros((len(qRrange_left),7))
+        nn_input_right = np.zeros((len(qRrange_right),7))
+        nn_input_left[:,6] = (np.log10(qRrange_left)-self.nn_minvalu_left[-1])/(self.nn_maxvalu_left[-1]-self.nn_minvalu_left[-1])
+        nn_input_right[:,6] = (qRrange_right-self.nn_minvalu_right[-1])/(self.nn_maxvalu_right[-1]-self.nn_minvalu_right[-1])
+        nn_input_left[:,:6] = (param[1:7]-self.nn_minvalu_left[:6])/(self.nn_maxvalu_left[:6]-self.nn_minvalu_left[:6])
+        nn_input_right[:,:6] = (param[1:7]-self.nn_minvalu_right[:6])/(self.nn_maxvalu_right[:6]-self.nn_minvalu_right[:6])
+        nn_output_left = np.array([10**i for i in self.model_left(nn_input_left).numpy()]).flatten()
+        nn_output_right = np.array([10**i for i in self.model_right(nn_input_right).numpy()]).flatten()
+        nn_output = np.hstack((nn_output_left,nn_output_right))
+        nn_output = nn_output + 10**(-param[-1])
+        return nn_output/nn_output[0]
+ 
+class scatterer_generator_log_log:
+    '''
+    shape specific descriptors (shape_params):
+    ------------------------------------------
+    *None.*
+
+    Input parameters to be predicted:
+    ------------------------------------------
+    *TODO*
+    '''
+    def __init__(self,shape_params = [NNMODEL_DIR_PATH+'/1.h5',NNMODEL_DIR_PATH+'/2.h5',5],
+                 minvalu = (50,0,0,0,0,0,0,6),
+                 maxvalu = (2000,1,1,1,1,1,1,7)):
+        self.minvalu = minvalu
+        self.maxvalu = maxvalu
+        self.numvars = 8
+        self.model_path_left = shape_params[0]
+        self.model_path_right = shape_params[1]
+        self.qRbreak = shape_params[2]
+
+    def load_model(self):
+        self.model_left = tf.keras.models.load_model(self.model_path_left)
+        self.nn_minvalu_left = np.array([0,0,0,0,0,0,-1])
+        self.nn_maxvalu_left = np.array([1,1,1,1,1,1,2])
+        self.model_right = tf.keras.models.load_model(self.model_path_right)
+        self.nn_minvalu_right = np.array([0,0,0,0,0,0,-1])
+        self.nn_maxvalu_right = np.array([1,1,1,1,1,1,2])
+
+    def converttoIQ(self,qrange,param):
+        self.load_model()
+        param = np.array(param)
+        R = param[0]
+        qRrange = np.array(qrange)*R
+        qRrange_left = qRrange[np.where(qRrange < self.qRbreak)]
+        qRrange_right = qRrange[np.where(qRrange >= self.qRbreak)]
+        nn_input_left = np.zeros((len(qRrange_left),7))
+        nn_input_right = np.zeros((len(qRrange_right),7))
+        nn_input_left[:,6] = (np.log10(qRrange_left)-self.nn_minvalu_left[-1])/(self.nn_maxvalu_left[-1]-self.nn_minvalu_left[-1])
+        nn_input_right[:,6] = (np.log10(qRrange_right)-self.nn_minvalu_right[-1])/(self.nn_maxvalu_right[-1]-self.nn_minvalu_right[-1])
+        nn_input_left[:,:6] = (param[1:7]-self.nn_minvalu_left[:6])/(self.nn_maxvalu_left[:6]-self.nn_minvalu_left[:6])
+        nn_input_right[:,:6] = (param[1:7]-self.nn_minvalu_right[:6])/(self.nn_maxvalu_right[:6]-self.nn_minvalu_right[:6])
+        nn_output_left = np.array([10**i for i in self.model_left(nn_input_left).numpy()]).flatten()
+        nn_output_right = np.array([10**i for i in self.model_right(nn_input_right).numpy()]).flatten()
+        nn_output = np.hstack((nn_output_left,nn_output_right))
+        nn_output = nn_output + 10**(-param[-1])
+        return nn_output/nn_output[0]
+ 
--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
                                            "vesiclenn_log_log = cga_vesiclenn.scatterer_generator:scatterer_generator_log_log",
                                            "vesiclenn_disp_nonvar = cga_vesiclenn_disp_nonvar.scatterer_generator:scatterer_generator",
                                            "vesiclenn_disp_fixta = cga_vesiclenn_disp_fixta.scatterer_generator:scatterer_generator",
+                                           "vesiclenn_disp_limitta = cga_vesiclenn_disp_limitta.scatterer_generator:scatterer_generator",
                                            ]},
         py_modules=["cga_vesiclenn"],
                         )
